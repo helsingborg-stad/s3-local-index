6a8ee5fdea89a31e39ecb911be3e3688a70b37b7
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-09-15 13:24:51 +0200

    fix: use cli init hook

--- s3-local-index.php
+++ s3-local-index.php
@@ -7,14 +7,14 @@
 
 use S3_Local_Index\CLI\Command;
 use S3_Local_Index\Stream\Wrapper;
+use WP_CLI;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-if (defined('WP_CLI') && WP_CLI) {
+add_action('cli_init', function () {
     WP_CLI::add_command('s3-index', Command::class);
-}
+});
 
-// Hook into plugins_loaded to ensure S3 is available
 add_action('plugins_loaded', function () {
     Wrapper::init();
 }, 20);
