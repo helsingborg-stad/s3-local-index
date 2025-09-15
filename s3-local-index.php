fb6b46f5620c7b31a551b25138ab9fcc3e6c6afb
GitHub Action <>
2025-09-15 10:57:19 +0000

    build: update package file versions to 0.1.4

--- s3-local-index.php
+++ s3-local-index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: S3 Local Index
  * Description: Provides local indexing of S3 files and a CLI command.
- * Version: 0.1.3
+ * Version: 0.1.4
  */
 
 use S3_Local_Index\CLI\Command;
