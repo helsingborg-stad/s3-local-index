bf80a743aeef92a7e2dd455bd827804763dbef35
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-10-01 12:54:09 +0200

    fix: add docs to WrapperOrg.

--- source/php/Stream/WrapperOriginal.php
+++ source/php/Stream/WrapperOriginal.php
@@ -4,6 +4,13 @@ namespace S3_Local_Index\Stream;
 use S3_Local_Index\Stream\WrapperInterface;
 use S3_Uploads\Stream_Wrapper as StreamWrapperOriginal;
 
+/**
+ * Original S3 stream wrapper class to delegate calls to.
+ * 
+ * This class extends the original S3 stream wrapper and implements
+ * the WrapperInterface to ensure compatibility with the custom
+ * stream wrapper implementation.
+ */
 class WrapperOriginal extends StreamWrapperOriginal implements WrapperInterface
 {
 }
