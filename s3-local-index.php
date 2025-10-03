d88798d5ce05c85fe602808753bf1964021b5179
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-10-03 12:43:38 +0200

    fix: remove obsolete docblock.

--- s3-local-index.php
+++ s3-local-index.php
@@ -3,11 +3,6 @@
  * Plugin Name: S3 Local Index
  * Description: Provides local indexing of S3 files and a CLI command with cache flushing capabilities.
  * Version: 0.2.100
- * 
- * CLI Commands:
- * - wp s3-index create                     : Create full S3 index
- * - wp s3-index flush [path] [--add]       : Flush cache for specific path, optionally add to rebuild list
- * - wp s3-index rebuild [--clear] [--all]  : Rebuild specific indexes from rebuild list
  */
 
 use S3_Local_Index\App;
